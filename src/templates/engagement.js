cd3914aa141b32930d12a0ec4d383ba6e87c6eb7
Scottiedog45 <scottiedog45@gmail.com>
2018-07-22 17:20:20 -0400

    fixing engagements

---
+++ src/templates/engagement.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import Helmet from 'react-helmet'
+// import Link from 'gatsby-link'
+// import Content, {HTMLContent} from '../components/Content'
+
+// export const EngagementTemplate = ({
+//   content, 
+//   contentComponent, 
+//   descript, 
+//   tags, 
+//   title, 
+//   helmet
+// }) => {
+//   const EngagementContent = contentComponent || Content
+
+//   return (
+//     <section className='section'>
+//     {helmet || ''}
+//     <div className='container content'>
+
+//     </div></section>
+//   )
+// }
