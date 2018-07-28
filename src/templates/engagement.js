70da83d89c6b330872710da734ec8199cef9b97c
Scottiedog45 <scottiedog45@gmail.com>
2018-07-27 23:20:13 -0400

    adding engagement stuff

--- src/templates/engagement.js
+++ src/templates/engagement.js
@@ -1,24 +1,55 @@
-// import React from 'react'
-// import PropTypes from 'prop-types'
-// import Helmet from 'react-helmet'
-// import Link from 'gatsby-link'
-// import Content, {HTMLContent} from '../components/Content'
+import React from 'react'
+import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
+import Content, {HTMLContent} from '../components/Content'
 
-// export const EngagementTemplate = ({
-//   content, 
-//   contentComponent, 
-//   descript, 
-//   tags, 
-//   title, 
-//   helmet
-// }) => {
-//   const EngagementContent = contentComponent || Content
+export const EngagementTemplate = ({
+  content, 
+  contentComponent, 
+  date, 
+  location, 
+  program, 
+  helmet
+}) => {
+  const EngagementContent = contentComponent || Content
 
-//   return (
-//     <section className='section'>
-//     {helmet || ''}
-//     <div className='container content'>
+  return (
+    <section className='section'>
+    {helmet || ''}
+    <div className='container content'>
+    <div>{date}</div>
+    <div>{location}</div>
+    <div>{program}</div>
+    </div>
+    </section>
+  )
+}
 
-//     </div></section>
-//   )
-// }
+const Engagement = ({ data }) => {
+  const { markdownRemark: engagement} = data
+
+  return (
+    <EngagementTemplate
+      content={engagement.html}
+      contentComponent={HTMLContent}
+      date = {engagement.frontmatter.date}
+      helmet={<Helmet title={`${engagement.frontmatter.date} | Concert`} />}
+      />
+  )
+}
+
+export default Engagement
+
+export const pageQuery = graphql`
+  query EngagementByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id 
+      htl
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        location
+        program
+      }
+    }
+  }`
