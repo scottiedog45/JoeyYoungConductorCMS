9048ddd1f7a570322f7d278b7f58ee3ecdb3d869
Scott O'Toole <scottiedog45@gmail.com>
2018-08-25 22:29:45 -0400

    adding link to config

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,7 +10,7 @@ import Navbar from '../components/Navbar'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => {
-  if (typeof window !== undefined) {
+  if (typeof window !== `undefined`) {
     return (
   <div>
     <Helmet title="Joseph Young" />
