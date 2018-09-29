58009dd3f3d5d015aeb1165c043245618fb12a33
Scott O'Toole <scottiedog45@gmail.com>
2018-09-29 09:32:50 +0200

    adding calendar page

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,76 +1,74 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import BackgroundImage from '../components/BackgroundImage'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import BackgroundImage from "../components/BackgroundImage";
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <div>
-        <div className='mobileBackgroundContainer' />
+        <div className="mobileBackgroundContainer" />
         <div className="calendarBackgroundContainer" />
         {/* <BackgroundImage className='calendarBackground' backgroundImage={this.props.data.calendarPageBackground} /> */}
-      <section className="section calendar-section">
-        <div className="container">
-          <div className="content">
-          <div className='styledTitle'>
-            CALENDAR
+        <section className="section calendar-section">
+          <div className="container">
+            <div className="content">
+              <div className="styledTitle">CALENDAR</div>
+            </div>
+            <div className="calendar-engagements-container">
+              {posts.map(({ node: post }) => (
+                <div
+                  className="content engagement-content"
+                  style={{ padding: "2em 4em" }}
+                  key={post.id}
+                >
+                  <p>
+                    <Link className="has-text-primary" to={post.fields.slug}>
+                      {post.frontmatter.date}
+                    </Link>
+                    <small>{post.frontmatter.dateTime}</small>
+                    <br />
+                    <small>{post.frontmatter.location}</small>
+                  </p>
+                  <p>
+                    {post.frontmatter.program}
+                    <br />
+                    <br />
+                    <Link to={post.frontmatter.eventLink}>
+                      more information...
+                    </Link>
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
-          <div className='calendar-engagements-container'>
-          {posts
-            .map(({ node: post }) => (
-              <div
-                className="content engagement-content"
-                style={{padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.date}
-                  </Link>
-                  <small>{post.frontmatter.dateTime}</small><br />
-                  <small>{post.frontmatter.location}</small>
-                </p>
-                <p>
-                  {post.frontmatter.program}
-                  <br />
-                  <br />
-                  <Link to={post.frontmatter.eventLink}>
-                    more information...
-                  </Link>
-                </p>
-              </div>
-            ))}
-            </div>
-        </div>
-      </section>
+        </section>
       </div>
-    )
+    );
   }
 }
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+      edges: PropTypes.array
+    })
+  })
+};
 
 export const pageQuery = graphql`
   query CalendarQuery {
-    calendarPageBackground: imageSharp(id: {regex: "/calendarPage/"}) {
-      sizes (maxWidth: 1240, quality: 100) {
+    calendarPageBackground: imageSharp(id: { regex: "/calendarPage/" }) {
+      sizes(maxWidth: 1240, quality: 100) {
         ...GatsbyImageSharpSizes_withWebp_noBase64
       }
     }
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "engagement" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "engagement" } } }
     ) {
       edges {
         node {
@@ -79,7 +77,7 @@ export const pageQuery = graphql`
           fields {
             slug
           }
-          frontmatter(draft: false) {
+          frontmatter(filter: { draft: false }) {
             eventLink
             location
             program
@@ -90,4 +88,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
