49e80b8f763950ebc7d5a67665ca38c9044f9aa8
Scott O'Toole <scottiedog45@gmail.com>
2018-09-30 23:19:41 +0200

    remove gatsby image links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Link from "gatsby-link";
 import NameHeader from "../components/NameHeader";
 import LandingQuote from "../components/LandingQuote";
-import Logo from "../components/Logo";
 
 export default class IndexPage extends React.Component {
   render() {
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import BackgroundImage from "../components/BackgroundImage";
 
 export default class IndexPage extends React.Component {
   render() {
